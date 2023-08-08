3c7a7005d04c222978fb2470dcb783c30a1cfda5
hanzhixiao <709674996@qq.com>
2023-08-08 11:19:14 +0800

    component check
    Signed-off-by: hanzhixiao <709674996@qq.com>

--- scripts/compose_check.sh
+++ scripts/compose_check.sh
@@ -3,4 +3,4 @@ SCRIPTS_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 OPENIM_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 
 echo checking composes...>>$OPENIM_ROOT/logs/openIM.log 2>&1&
-go build -o OPENIM_ROOT/ $SCRIPTS_ROOT/check/check_compose.go
+go build $SCRIPTS_ROOT/check/check_compose.go
